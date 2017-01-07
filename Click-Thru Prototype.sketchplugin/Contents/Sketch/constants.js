5bab684e1622eeea3b3a2afa862c8e076ee61d42
Mark Horgan <public@markhorgan.com>
2017-01-07 09:56:19 +0000

    Minor refactoring of constants

--- Click-Thru Prototype.sketchplugin/Contents/Sketch/constants.js
+++ Click-Thru Prototype.sketchplugin/Contents/Sketch/constants.js
@@ -1,5 +1,6 @@
 var Constants = {}
 
+// keys used on layers
 Constants.ARTBOARD_LINK = "artboardLink"
 Constants.EXTERNAL_LINK = "externalLink"
 Constants.OPEN_LINK_IN_NEW_WINDOW = "openLinkInNewWindow"
@@ -8,11 +9,11 @@ Constants.IS_MOBILE_MENU = 'isMobileMenu'
 Constants.DIALOG_TYPE = 'dialogType'
 Constants.DIALOG_TEXT = 'dialogText'
 
-Constants.DIALOG_TYPE_ALERT = 'alert'
-Constants.DIALOG_TYPE_CONFIRM = 'confirm'
-
 Constants.KEYS = [Constants.ARTBOARD_LINK, Constants.IS_MOBILE_MENU_BUTTON, Constants.IS_MOBILE_MENU, Constants.DIALOG_TYPE,
   Constants.DIALOG_TEXT]
+
+Constants.DIALOG_TYPE_ALERT = 'alert'
+Constants.DIALOG_TYPE_CONFIRM = 'confirm'
 
 Constants.TAB_SIZE = 2
 Constants.HOTSPOT_PADDING = 10
