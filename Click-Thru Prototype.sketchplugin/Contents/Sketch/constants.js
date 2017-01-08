53b6241de914c6a89fc2b47256c6092f90298f89
Mark Horgan <public@markhorgan.com>
2017-01-08 09:34:29 +0000

    Refactored constants

--- Click-Thru Prototype.sketchplugin/Contents/Sketch/constants.js
+++ Click-Thru Prototype.sketchplugin/Contents/Sketch/constants.js
@@ -1,19 +1,23 @@
-var Constants = {}
-
-// keys used on layers
-Constants.ARTBOARD_LINK = "artboardLink"
-Constants.EXTERNAL_LINK = "externalLink"
-Constants.OPEN_LINK_IN_NEW_WINDOW = "openLinkInNewWindow"
-Constants.IS_MOBILE_MENU_BUTTON = 'isMobileMenuButton'
-Constants.IS_MOBILE_MENU = 'isMobileMenu'
-Constants.DIALOG_TYPE = 'dialogType'
-Constants.DIALOG_TEXT = 'dialogText'
+var Constants = {
+  ARTBOARD_LINK: "artboardLink",
+  EXTERNAL_LINK: "externalLink",
+  OPEN_LINK_IN_NEW_WINDOW: "openLinkInNewWindow",
+  IS_MOBILE_MENU_BUTTON: "isMobileMenuButton",
+  IS_MOBILE_MENU: "isMobileMenu",
+  DIALOG_TYPE: "dialogType",
+  DIALOG_TEXT: "dialogText",
+  DIALOG_TYPE_ALERT: "alert",
+  DIALOG_TYPE_CONFIRM: "confirm",
+  TAB_SIZE: 2,
+  HOTSPOT_PADDING: 10
+}
 
 Constants.KEYS = [Constants.ARTBOARD_LINK, Constants.IS_MOBILE_MENU_BUTTON, Constants.IS_MOBILE_MENU, Constants.DIALOG_TYPE,
   Constants.DIALOG_TEXT]
 
-Constants.DIALOG_TYPE_ALERT = 'alert'
-Constants.DIALOG_TYPE_CONFIRM = 'confirm'
-
-Constants.TAB_SIZE = 2
-Constants.HOTSPOT_PADDING = 10
+var ResizingType = {
+  STRETCH: 0,
+  PIN_TO_CORNER: 1,
+  RESIZE_OBJECT: 2,
+  FLOAT_IN_PLACE: 3
+}
